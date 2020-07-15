01e147746ab5f9b59eeb6b83876610448f7fa422
BinhBinh1812 <68274384+BinhBinh1812@users.noreply.github.com>
2020-07-15 18:04:08 +0700

    Add files via upload

---
+++ PhanSo.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace GiuaKy
+{
+    public class PhanSo
+    {
+        private Int32 tuSo;
+        private Int32 mauSo;
+
+        public Int32 TuSo
+        {
+            get { return tuSo; }
+            set { tuSo = value; }
+        }
+        public Int32 MauSo
+        {
+            get { return mauSo; }
+            set
+            {
+                if (value != 0)
+                    mauSo = value;
+            }
+
+        }
+        public void Nhap()
+        {
+            Console.WriteLine("Mời bạn nhập tử số: ");
+            tuSo = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Mời bạn nhập mẫu số: ");
+                mauSo = Convert.ToInt32(Console.ReadLine());
+                if (mauSo == 0)
+                {
+                    Console.WriteLine("Mẫu số phải khác 0. Vui lòng nhập lại!");
+                }
+            } while (mauSo == 0);
+
+        }
+        public void Nhap1tx()
+        {
+            Console.WriteLine("Mời bạn nhập tử số: ");
+            TuSo = Convert.ToInt32(Console.ReadLine());
+        }
+        //Hàm khởi tạo mặc định
+        public PhanSo()
+        {
+            this.tuSo = 0;
+            this.mauSo = 1;
+        }
+        //Hàm khởi tạo 2 tham số
+
+        public PhanSo(Int32 TuSo, Int32 MauSo)
+        {
+            this.tuSo = TuSo;
+            this.mauSo = MauSo;
+        }
+
+        //Hàm khởi tạo 1 tham số
+
+        public PhanSo(Int32 x)
+        {
+            this.tuSo = x;
+            this.mauSo = 1;
+        }
+
+
+        //Cộng hai phân số
+        public static PhanSo Cong(PhanSo a, PhanSo b)
+        {
+            PhanSo Cong = new PhanSo();
+
+            Cong.tuSo = a.tuSo * b.mauSo + a.mauSo * b.tuSo;
+            Cong.mauSo = a.mauSo * b.mauSo;
+            return Cong;
+        }
+
+        //Trừ hai phân số
+        public static PhanSo Tru(PhanSo a, PhanSo b)
+        {
+            PhanSo Tru = new PhanSo();
+            Tru.tuSo = a.tuSo * b.mauSo - a.mauSo * b.tuSo;
+            Tru.mauSo = a.mauSo * b.mauSo;
+            return Tru;
+        }
+
+        //Xuất phân số
+        public void Xuat()
+        {
+            //if (mauSo == 1)
+            // Console.WriteLine(tuSo);
+            //else
+            Console.WriteLine(tuSo + "/" + mauSo);
+        }
+
+        public Int32 USLN(Int32 a, Int32 b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return a + b;
+            }
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return a;
+        }
+        public void RutGon()
+        {
+            Int32 uscl = USLN(Math.Abs(tuSo), Math.Abs(mauSo));
+            tuSo = tuSo / uscl;
+            mauSo = mauSo / uscl;
+        }
+
+        //Giá trị số thực của phân số
+        //public static PhanSo GiaTri(PhanSo a,PhanSo b)
+        //{
+        //    float tong = a.tuSo / a.mauSo;
+        //    float hieu = b.tuSo / b.mauSo;
+        //    return 0;
+        //}
+
+        //phân số nghịch đảo
+        public void NghichDao(PhanSo a)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuSo = a.mauSo;
+            kq.mauSo = a.tuSo;
+
+        }
+        public override string ToString()
+        {
+            return string.Format(tuSo + "/" + mauSo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GiuaKy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            PhanSo macdinh = new PhanSo();
+            PhanSo xts = new PhanSo();
+            PhanSo ps1 = new PhanSo();
+            PhanSo ps2 = new PhanSo();
+            PhanSo tong = new PhanSo();
+            PhanSo hieu = new PhanSo();
+
+            // PhanSo giatri = new PhanSo();
+
+
+
+
+            Console.WriteLine("Nhập phân số thứ 1: ");
+            ps1.Nhap();
+            Console.WriteLine("Nhập phân số thứ 2: ");
+            ps2.Nhap();
+            Console.WriteLine("Nhập phân số thứ 3:");
+            xts.Nhap1tx();
+
+            macdinh.ToString();
+            Console.WriteLine("Phân số mặc định là:" + macdinh.ToString());
+            xts.ToString();
+            Console.WriteLine("Khởi tạo 1 tham số là:" + xts.ToString());
+            ps1.ToString();
+            ps1.RutGon();
+
+
+
+            Console.WriteLine("Phân số đầu tiên là:" + ps1.ToString());
+            ps1.ToString();
+            ps1.RutGon();
+
+            Console.WriteLine("Phân số thứ hai là:" + ps2.ToString());
+            ps2.ToString();
+            ps2.RutGon();
+
+
+
+            tong = PhanSo.Cong(ps1, ps2);
+            ps1.ToString();
+            Console.Write(" cộng ");
+            ps2.ToString();
+            Console.Write(" = ");
+            tong.RutGon();
+            tong.Xuat();
+            Console.WriteLine();
+
+            hieu = PhanSo.Tru(ps1, ps2);
+            ps1.ToString();
+            Console.Write(" trừ ");
+            ps2.ToString();
+            Console.Write(" = ");
+            hieu.RutGon();
+            hieu.Xuat();
+            Console.WriteLine();
+
+
+
+            //tong = PhanSo.GiaTri(ps1, ps2);
+            //ps1.ToString();
+            //Console.WriteLine("Giá trị trả về số thực là:");
+
+            //hieu = PhanSo.GiaTri(ps1, ps2);
+            //ps2.ToString();
+            //Console.WriteLine("Giá trị trả về số thực là:");
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
